d173edc744240a74a83d4c1b52ec6ac3351fc90d
paul-Bill <paulsongustaf831@gmail.com>
2023-08-17 02:07:17 +0800

    small error
    Co-Authored-By: Yeo Kenneth <141793959+YeoKenneth@users.noreply.github.com>
    Co-Authored-By: Edgar Tashord Potter <135199386+edward-potter818@users.noreply.github.com>
    Co-Authored-By: DamonFinch <132647647+DamonFinch@users.noreply.github.com>

--- src/components/AllDataView.js
+++ src/components/AllDataView.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import AllDataMapView from "../features/mapView/AllDataMapView";
 import { DownloadOutlined } from '@ant-design/icons';
 import { toggleView2 } from "../features/filter/StateReducer";
 import App from './setting'
 import { Button, Row } from "antd";
+import { hide } from "../features/filter/StateReducer";
 const Datas = () => {
   const loading = useSelector((state) => state.data.loading, []);
   const dispatch = useDispatch();
   const setView1 = () => {
     dispatch(toggleView2());
   };
+  
+  useEffect(() => {
+    dispatch(hide());
+  }, [dispatch]);
   return (
     <Row style={{ height: "100%" }}>
-      <Button type="primary" onClick={setView1} icon={<DownloadOutlined />}  loading={loading}  className='setting_btn'>
-        {loading?'Loading...':'SelectData'}
+      <Button type="primary" onClick={setView1} icon={<DownloadOutlined />} loading={loading} className='setting_btn'>
+        {loading ? 'Loading...' : 'SelectData'}
       </Button>
       <App />
       <AllDataMapView />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import MapView from "../features/mapView/MapView";
 import App from './setting'
@@ -6,16 +6,20 @@ import { useSelector } from 'react-redux';
 import { Button, Row } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
 import { toggleView1 } from "../features/filter/StateReducer";
+import { hide } from "../features/filter/StateReducer";
 const Home = () => {
   const dispatch = useDispatch();
   const loading = useSelector((state) => state.filter.loading, []);
   const setView1 = () => {
     dispatch(toggleView1());
   };
+  useEffect(() => {
+    dispatch(hide());
+  }, [dispatch]);
   return (
     <Row style={{ height: "100%", width: '100%' }}>
-      <Button type="primary" onClick={setView1} icon={<DownloadOutlined /> } loading={loading} className='setting_btn'>
-       {loading?'Loading...':'DefineData'}
+      <Button type="primary" onClick={setView1} icon={<DownloadOutlined />} loading={loading} className='setting_btn'>
+        {loading ? 'Loading...' : 'DefineData'}
       </Button>
       <App />
       <MapView />
