244e6a41590966ad49fbc3f24a3fc8f70224780c
paul-Bill <paulsongustaf831@gmail.com>
2023-08-18 14:32:25 +0800

    import button for showing option modal

---
+++ src/components/SearchOption/ButtonGlow/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import './ButtonGlow.css'
+
+const ButtonGlow = () => {
+    <div class="option_area">
+        <div class="option_toggle">
+            <div class="option_icon">
+                <div class="option_horizontal"></div>
+                <div class="option_vertical"></div>
+            </div>
+        </div>
+        <div class="option_bubble"></div>
+    </div>
+}
+
+export default ButtonGlow
---
+++ src/components/SearchOption/OptionCard.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { Modal } from 'antd'
+
+const OptionCard = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+      setIsModalOpen(true);
+    };
+    const handleOk = () => {
+      setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };
+}
+
+export default OptionCard
